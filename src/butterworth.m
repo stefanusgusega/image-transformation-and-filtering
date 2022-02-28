0012fed3a632755f6adb7170c15214aaa2a28596
Annisa Rahim <13518089@std.stei.itb.ac.id>
2022-03-01 00:02:00 +0700

    feat: add LPF function

--- src/butterworth.m
+++ src/butterworth.m
@@ -1,6 +1,6 @@
 function [filter_arr] = butterworth(D, cutoff_freq, filter_order)
     %BUTTERWORTH Generate Butterworth filter array
 
-    filter_arr = 1 / (1 + (D / cutoff_freq)^(2 * filter_order))
+    filter_arr = 1 ./ (1 + (D ./ cutoff_freq).^(2 * filter_order));
 
 end
--- src/generate_filter.m
+++ src/generate_filter.m
@@ -1,10 +1,6 @@
 function [filter_arr] = generate_filter(p, q, cutoff_freq, name, filter_order)
     %GENERATE_FILTER Generate filter
 
-    if (strcmp(name, 'butterworth') & ~exist(filter_order));
-        throw(MException('FilterOrder:variableUndefined', 'You should define filter order if using Butterworth filter.'))
-    end
-
     % Setup range of variables
     u = 0:(p - 1);
     v = 0:(q - 1);
