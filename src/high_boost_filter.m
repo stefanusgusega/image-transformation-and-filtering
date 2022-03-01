390da6d8896109baadc5bca19f704406ccdaa135
stefanusgusega <gunawanstefanus006@gmail.com>
2022-03-01 23:47:25 +0700

    feat: add im2double and switch case for filter name

--- src/high_boost_filter.m
+++ src/high_boost_filter.m
@@ -1,5 +1,14 @@
-function [imgOut] = high_boost_filter(imgIn, amplification_coef, filter_name, filter_order, cutoff_freq)
+function [imgOut] = high_boost_filter(imgIn, amplification_coef, filter_name, cutoff_freq, filter_order)
     %High Boost Filter. This function is to make an image brighter by doing amplification on original image and then substract with lowpass filter
-    imgOut = amplification_coef .* imgIn - pass_filter(imgIn, 'low', filter_name, filter_order, cutoff_freq)
+    im = im2double(imgIn);
+
+    switch filter_name
+        case 'butterworth'
+            pf = pass_filter(imgIn, 'low', filter_name, cutoff_freq, filter_order);
+        otherwise
+            pf = pass_filter(imgIn, 'low', filter_name, cutoff_freq);
+    end
+
+    imgOut = amplification_coef .* im - pf;
 
 end
