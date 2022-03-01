1520adbb91144de3da8e75d644e73f9864953741
stefanusgusega <gunawanstefanus006@gmail.com>
2022-03-01 21:59:32 +0700

    feat: add filter name and pass type checker

--- src/pass_filter.m
+++ src/pass_filter.m
@@ -2,8 +2,20 @@ function [imgOut] = pass_filter(imgIn, pass_type, filter_name, filter_order, cut
     %LPF lowpass filter function
     [M, N, D] = size(imgIn);
 
+    % Checking the inputs
+    % Check dimension
     if ~(D == 1)
         throw(MException('ImageError:sizeNotOne', 'The input image should be 1D array. Current: %dD array.', D))
+    end
+
+    % Check pass type
+    if ~(ismember(pass_type, {'high' 'low'}))
+        throw(MException('PassTypeError:invalidPassType', "Pass type defined: 'high' or 'low'. %s not defined.", pass_type))
+    end
+
+    % Check filter name
+    if ~(ismember(filter_name, {'ideal' 'butterworth' 'gaussian'}))
+        throw(MException('FilterNameError:invalidFilterName', "Filter name defined: 'ideal', 'butetrworth' or 'gaussian'. %s not defined.", filter_name))
     end
 
     % Step 1
