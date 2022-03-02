27cac692e549b6c95d920c6a9ffb9da9c0f42c8c
stefanusgusega <gunawanstefanus006@gmail.com>
2022-03-02 23:38:33 +0700

    fix: change to frequency based high boost filter

--- src/high_boost_filter.m
+++ src/high_boost_filter.m
@@ -2,6 +2,9 @@ function [imgOut] = high_boost_filter(imgIn, amplification_coef, filter_name, cu
     %High Boost Filter. This function is to make an image brighter by doing amplification on original image and then substract with lowpass filter
     im = im2double(imgIn);
 
+    % Get the Fourier spectrum of the image
+    fourier_spectrum = fourier_spectrum(im)
+
     switch filter_name
         case 'butterworth'
             pf = pass_filter(imgIn, 'low', filter_name, cutoff_freq, filter_order);
@@ -9,6 +12,8 @@ function [imgOut] = high_boost_filter(imgIn, amplification_coef, filter_name, cu
             pf = pass_filter(imgIn, 'low', filter_name, cutoff_freq);
     end
 
-    imgOut = amplification_coef .* im - pf;
+    filter_with_amp = amplification_coef - pf;
+
+    imgOut = filter_with_amp .* fourier_spectrum;
 
 end
