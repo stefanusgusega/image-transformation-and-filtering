bb53f733d8740e79e79e9a497c791dbe2869f880
Annisa Rahim <13518089@std.stei.itb.ac.id>
2022-03-02 18:41:23 +0700

    fix: fourier spectrum

--- src/fourier_spectrum.m
+++ src/fourier_spectrum.m
@@ -1,13 +1,25 @@
 function [f,F2] = fourier_spectrum(imgIn)
     % Tampilkan magnitute spektrum Fourier, mengembalikan hasil fft +
     % spectrum untuk didisplay
+    [M, N, D] = size(imgIn);
+
+    % Check dimension
+    if ~(D == 1)
+        throw(MException('ImageError:sizeNotOne', 'The input image should be 1D array. Current: %dD array.', D))
+    end
+
     im = im2double(imgIn);
     f = fft2(im);
     f = fftshift(f);
-
+    disp(size(f));
+    
     F2 = f;
     F2 = abs(F2); % Get the magnitude
     F2 = log(1+F2); % Use log
+
+    %disp(size(F2));
+    %disp(min(F2,[],'all'));
+    %disp(max(F2,[],'all'));
     figure, imshow(F2,[]);
     title('magnitude spectrum');
 end
