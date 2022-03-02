82b4fe2bd2a251e4a87465201b37ecd825e370d1
Annisa Rahim <13518089@std.stei.itb.ac.id>
2022-03-02 14:21:55 +0700

    add notch & banderject file

---
+++ src/banderject.m
@@ -0,0 +1,5 @@
+function [imgOut] = banderject(imgIn, filter_name, C0, W, n)
+    %Banderject filter function
+    [M, N, D] = size(imgIn);
+    
+end
---
+++ src/notch_filter.m
@@ -0,0 +1,5 @@
+function [imgOut] = notch_filter(imgIn, arr_noise)
+    %LPF lowpass filter function
+    [M, N, D] = size(imgIn);
+
+end
