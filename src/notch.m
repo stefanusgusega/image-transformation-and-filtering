99c5d8ce82a23fafd053e565db9a8a2e665ef296
Annisa Rahim <13518089@std.stei.itb.ac.id>
2022-03-03 18:49:55 +0700

    feat: noise removal for image a b c

--- src/notch.m
+++ src/notch.m
@@ -11,20 +11,15 @@ function [imgOut] = notch(imgIn, noise_type)
         noise_type = 'z';
     end
 
-    figure, imshow(imgIn);
+    %figure, imshow(imgIn);
 
-    % transform image to freq spectrum
+    % Mengubah gambar ke ranah frekuensi
     im = im2double(imgIn);
     f = fft2(im);
     freqIn = fftshift(f);
 
     switch noise_type
         case 'a'
-            %arr_x  = [180, 180, 180, 200, 200, 200,  73, 328,  52, 307];
-            %arr_x1 = [184, 184, 184, 204, 204, 204,  77, 332,  56, 311];
-            %arr_y  = [ 52, 180, 308,  73, 200, 328, 200, 200, 180, 180];
-            %arr_y1 = [ 56, 184, 310,  77, 204, 332, 204, 204, 184, 184];
-
             arr_x  = [177, 177, 177, 197, 197, 197,  70, 325,  49, 304];
             arr_x1 = [187, 187, 187, 207, 207, 207,  80, 335,  59, 314];
             arr_y  = [ 49, 177, 304,  70, 197, 325, 197, 197, 177, 177];
@@ -37,20 +32,23 @@ function [imgOut] = notch(imgIn, noise_type)
             arr_y1 = [140, 140, 140, 140, 140, 140,  10,  10,  70,  70, 405, 405, 405, 405, 405, 405, 475, 475, 525, 525];
         
         case 'c'
-            arr_x  = [ 27,  85, 150, 355, 410, 480, 155, 355, 155, 355,  30,  95, 155, 350, 420, 480, 150, 355, 150, 355];
-            arr_x1 = [ 37, 105, 170, 365, 430, 500, 170, 365, 170, 365,  50, 115, 175, 370, 440, 510, 170, 375, 170, 375];
-            arr_y  = [120, 120, 120, 130, 130, 130,   1,   1,  55,  55, 385, 385, 385, 385, 385, 385, 455, 455, 520, 520];
-            arr_y1 = [140, 140, 140, 140, 140, 140,  10,  10,  70,  70, 405, 405, 405, 405, 405, 405, 475, 475, 525, 525];
+            arr_y  = [ 26,  42,  58,  74,  90, 106, 122, 153, 170, 185, 202, 218, 234, 250];
+            arr_y1 = [ 31,  47,  63,  79,  94, 111, 126, 157, 174, 191, 206, 222, 238, 254];
+            arr_x  = [252, 236, 218, 203, 187, 171, 155, 122, 107,  91,  75,  59,  43,  27];
+            arr_x1 = [254, 238, 224, 207, 191, 175, 159, 126, 111,  95,  79,  63,  47,  31];
+
+        case 'z'
+            % ambil dari input user
             
     end
 
-
+    % set semua pixel pada range menjadi 0
     freqOut = notch_filter(freqIn, arr_x, arr_x1, arr_y, arr_y1);
     
-    display_spectrum(freqIn);
-    display_spectrum(freqOut);
+    %display_spectrum(freqIn);
+    %display_spectrum(freqOut);
 
     % kembali ke ranah spasial
     imgOut = real(ifft2(ifftshift(freqOut)));
-    figure, imshow(imgOut,[]);
+    %figure, imshow(imgOut,[]);
 end
